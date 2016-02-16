496ad41e3b49343694228b23831551374a55ab1e
Alex O'Donovan <aodonovan@fexco.com>
2016-02-16 10:12:53 +0000

    hello worlf

---
+++ src/main/java/com/fexco/fxol/FxolApplication.java
@@ -0,0 +1,12 @@
+package com.fexco.fxol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxolApplication.class, args);
+	}
+}
